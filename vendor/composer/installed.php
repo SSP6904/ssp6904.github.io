be23c2d196660a8ddcfce47a550a4676dcf3eb13
SSP6904 <75172901+SSP6904@users.noreply.github.com>
2023-03-16 18:53:16 +0000

    Files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6eb3e7d1c52110a369fe449a137e888d75e2e1f2',
+    'name' => 'craftcms/redactor',
+  ),
+  'versions' => 
+  array (
+    'craftcms/redactor' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6eb3e7d1c52110a369fe449a137e888d75e2e1f2',
+    ),
+    'yiisoft/yii2-composer' => 
+    array (
+      'pretty_version' => '2.0.10',
+      'version' => '2.0.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '94bb3f66e779e2774f8776d6e1bdeab402940510',
+    ),
+  ),
+);
